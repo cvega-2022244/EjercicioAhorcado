b0db0b086781eb0bba3cd7e4897fd59698713414
cvega-2022244 <134987444+cvega-2022244@users.noreply.github.com>
2024-01-19 09:21:55 -0600

    subo quinto push

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function actualizarDisplayPalabra() {
     displayPalabra.textContent = palabraAdivinada.join(' ');
 }
 
+function adivinarLetra(letra) {
+    letra = letra.toLowerCase();
+
+    if (palabraAdivinada.includes(letra)) {
+        mensajeDisplay.textContent = 'Ya has intentado esa letra.';
+        return;
+    }
+
+    if (palabraSeleccionada.includes(letra)) {
+        palabraSeleccionada.split('').forEach((char, index) => {
+            if (char === letra) {
+                palabraAdivinada[index] = letra;
+            }
+        });
+        actualizarDisplayPalabra();
+        checkGanar();
+    } else {
+        intentos++;
+        actualizarMensaje();
+        checkPerder();
+    }
+}
+
